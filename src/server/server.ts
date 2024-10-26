b0802a347d255bb75e60455a858c525379c07b3e
antonsokolovsky <anton.a.sokolovsky@gmail.com>
2024-10-26 15:53:29 +0300

    feat: add basic web socket server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,1 +1,17 @@
-PORT = 4000;
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+console.log('WebSocket server started on ws://localhost:8080');
+
+wss.on('connection', (ws) => {
+    console.log('New client connected');
+
+    ws.on('message', (message) => {
+        console.log('Received:', message);
+        ws.send(JSON.stringify({ type: 'ack', data: 'Message received' }));
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
